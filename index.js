3476b458eed913c1364691cef1ac5d0b982df425
Nick Lisauskas <nlisausk@alumni.nd.edu>
2018-04-22 19:56:49 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,6 @@
-const app = "I don't do much.";
+const token = '62ca5ef9c2883fbedb54924265b50e8d77023cc3'
+fetch('https://api.github.com/user/repos', {
+  headers: {
+    Authorization: `token ${token}`
+  }
+}).then(res => res.json()).then(json => console.log(json));
